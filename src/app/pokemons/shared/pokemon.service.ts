8bc9bd41e41187fb02a2eabf317f718e9a544207
DESKTOP-4GUMU4J\Amin <min03041999@gmail.com>
2021-09-03 13:41:09 +0700

    feat(pokemon-list): add pokemon list

---
+++ src/app/pokemons/shared/pokemon.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Pokemon } from './pokemon.model';
+import data from './../../../assets/pokedex.json';
+import { Observable, of } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+
+  constructor() { }
+  getPokemonList(): Observable<Pokemon[]>{
+    const pokemons = of(data.data);
+    return pokemons;
+  }
+}
