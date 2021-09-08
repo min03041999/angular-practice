1a4ec2d1c38638192c5d55eab6a6de3132df1b97
DESKTOP-4GUMU4J\Amin <min03041999@gmail.com>
2021-09-08 12:32:10 +0700

    feat(pokemon-detail): add pokemon detail

--- src/app/pokemons/pokemons.module.ts
+++ src/app/pokemons/pokemons.module.ts
@@ -2,20 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PokemonsRoutingModule } from './pokemons-routing.module';
+
+//Component
 import { PokemonsComponent } from './pokemons.component';
 import { PokemonSearchComponent } from './components/pokemon-search/pokemon-search.component';
+import { PokemonDetailComponent } from './components/pokemon-detail/pokemon-detail.component';
+
 
 import { FormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     PokemonsComponent,
-    PokemonSearchComponent
+    PokemonSearchComponent,
+    PokemonDetailComponent
   ],
   imports: [
     CommonModule,
     PokemonsRoutingModule,
-    FormsModule
+    FormsModule,
+    MatDialogModule,
+    MatIconModule
   ]
 })
 export class PokemonsModule { }
